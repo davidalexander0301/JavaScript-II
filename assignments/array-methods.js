3b084061c93e4326771d6605d7179ecd49499f9e
davidalexander0301 <davidalexander0301@gmail.com>
2019-02-26 17:27:11 -0600

    david-thompson

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,21 +56,29 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+runners.forEach((rep) => {fullName.push(rep["first_name"] + " " + rep["last_name"]);}); 
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
+let firstNames = [];
+runners.forEach((rep) => {firstNames.push(rep["first_name"]);});
+allCaps = firstNames.map(a => a.toUpperCase());
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
+largeShirts = runners.filter(rep => rep["shirt_size"] === "L");
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
+let amounts = [];
+runners.forEach(rep => {amounts.push(rep["donation"]);});
+ticketPriceTotal = amounts.reduce((accumulator, currentValue) => {return accumulator + currentValue;});
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
@@ -78,6 +86,18 @@ console.log(ticketPriceTotal);
 
 // Problem 1
 
+let companyNames = [];
+runners.forEach((rep) => {companyNames.push(rep["company_name"]);}); 
+console.log(companyNames);
+
 // Problem 2
 
+let nameDonation = [];
+runners.forEach((rep) => {nameDonation.push(rep["company_name"] + " " + rep["donation"]);}); 
+console.log(nameDonation);
+
 // Problem 3
+
+let donatedOverAHundred = [];
+donatedOverAHundred = runners.filter(rep => rep["donation"] >= 100);
+console.log(donatedOverAHundred);
